fddb357f765cba8d56366ca71076f5277526f833
Nheil3Dev <98476350+Nheil3Dev@users.noreply.github.com>
2023-10-21 14:01:18 +0200

    socket io server implemented

--- index.js
+++ index.js
@@ -3,26 +3,82 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import loader from 'morgan'
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
 
 dotenv.config()
+const PORT = process.env.PORT ?? 1234
+
 const app = express()
 const router = express.Router()
-const bd = createClient({
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:5173'
+  }
+})
+
+const db = createClient({
   url: 'libsql://grown-pestilence-nheil3dev.turso.io',
   authToken: process.env.DB_TOKEN
 })
 
-const PORT = process.env.PORT ?? 1234
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS whatsappclone (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user TEXT,
+    content TEXT,
+    date TIMESTAMP
+  )
+`)
 
+io.on('connection', async (socket) => {
+  console.log('A user has connected!')
+
+  socket.on('disconnect', () => {
+    console.log('An user has disconnected')
+  })
+
+  socket.on('whatsapp clone msg', async (msg, date) => {
+    const user = socket.handshake.auth.user ?? 'Anonymous'
+    let result
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO whatsappclone (user, content, date) VALUES (?, ?, ?)',
+        args: [user, msg, date]
+      })
+    } catch (e) {
+      console.error(e)
+    }
+    io.emit('whatsapp clone msg', result.lastInsertRowid.toString(), user, msg, date)
+  })
+
+  if (!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: 'SELECT * FROM whatsappclone WHERE id > ?',
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+      results.rows.forEach(({ id, user, content, date }) => {
+        socket.emit('whatsapp clone msg', id.toString(), user, content, date)
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+})
+
+// Middlewares
 app.use(cors())
 app.use(express.json())
 app.use(loader('dev'))
 
+// Routers
 app.use('/api', router)
 
 router.get('/messages', async (req, res) => {
   try {
-    const messages = await bd.execute('SELECT * FROM messages')
+    const messages = await db.execute('SELECT * FROM messages')
     res.json(messages.rows)
   } catch (e) {
     console.error(e)
@@ -31,14 +87,13 @@ router.get('/messages', async (req, res) => {
 
 router.get('/lastMsg', async (req, res) => {
   try {
-    const lastMsg = await bd.execute('SELECT * FROM messages ORDER BY id DESC LIMIT 1')
-    console.log(lastMsg.rows[0])
+    const lastMsg = await db.execute('SELECT * FROM whatsappclone ORDER BY id DESC LIMIT 1')
     res.json(lastMsg.rows[0])
   } catch (e) {
     console.error(e)
   }
 })
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Escuchando en: http://localhost:${PORT}`)
 })
